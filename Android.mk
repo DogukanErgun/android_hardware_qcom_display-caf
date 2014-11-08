8a85238ee406e9644c8a26c2a58e723b17e335c2
Dogukan Ergun <dogukan.ergun@gmail.com>
2014-11-08 17:05:44 +0200

    Change android.mk for aosp

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
-ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf)
+
+ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
 
 display-hals := libgralloc libgenlock libcopybit libvirtual
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
@@ -8,12 +9,7 @@ ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
 
-ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
-    include $(call all-named-subdir-makefiles,$(display-hals))
-else
-ifneq ($(filter msm8226 msm8x26 msm8960 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
-    #This is for mako since it doesn't have the QCOM make functions
-    include $(call all-named-subdir-makefiles,$(display-hals))
+
+include $(call all-named-subdir-makefiles,$(display-hals))
 endif
-endif
-endif
+
